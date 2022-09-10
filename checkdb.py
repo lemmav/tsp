9f23565a627fec244c530750da9b2a54a34e8881
lemmav <v.aem@yandex.ru>
2022-09-10 19:46:08 +0300

    Move from progress to result

--- checkdb.py
+++ checkdb.py
@@ -1,7 +1,7 @@
 import sqlite3, datetime
 import time, requests
 algorithms = {'GA':'http://127.0.0.1:5001', 'BNN': 'http://127.0.0.1:5001', 'NN': 'http://127.0.0.1:5001'}
-def read_sqlite_table():
+def fromqueuetoprogress():
     try:
         sqlite_connection= sqlite3.connect('createdbs.sqlite', timeout=20)
         sqlite_connection.row_factory = sqlite3.Row
@@ -30,8 +30,40 @@ def read_sqlite_table():
             sqlite_connection.close()
             print("Соединение с SQLite закрыто")
 
+def donecheck():
+    try:
+        sqlite_connection= sqlite3.connect('createdbs.sqlite', timeout=20)
+        sqlite_connection.row_factory = sqlite3.Row
+        cursor = sqlite_connection.cursor()
+        print("Подключен к SQLite")
+        for algorithm in algorithms.keys():
+            cursor.execute("SELECT * FROM progress WHERE algorithm=?", (algorithm,))
+            progressrow = cursor.fetchone()
+            print("SELECTED FROM progress")
+            if progressrow is not None:
+                progressrequest = requests.get(algorithms[algorithm]+'/progress')
+                if int(float(progressrequest.text))==1:
+                    resultrequest = requests.get(algorithms[algorithm]+'/result')
+                    resultrequest_str = resultrequest.text
+                    resultrequest_str = resultrequest_str.split('\n')
+                    print(resultrequest_str[-1])
+                    cursor.execute("INSERT INTO result (url, answer, algorithm, executiontime) VALUES (?, ?, ?, datetime('now', 'localtime')) ", (progressrow['url'], resultrequest_str[-1], progressrow['algorithm']))
+                    cursor.execute("DELETE FROM progress WHERE url=?", (progressrow['url'],))
+                    print("INSERTED INTO result and DELETED FROM progress")
+                    sqlite_connection.commit()
+
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка при подключении к sqlite", error)
+    finally:
+        if (sqlite_connection):
+            sqlite_connection.close()
+            print("Соединение с SQLite закрыто")
 
 while(True):
-    read_sqlite_table()
-    print ("I'm infinity")
+    fromqueuetoprogress()
+    
+    donecheck()
+    print ("I'm in infinity")
     time.sleep(5)
